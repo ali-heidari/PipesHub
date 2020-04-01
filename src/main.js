cd7eae17b1cd177ed5c3fdf7cd344f6f29465172
ali <ali-heidari@outlook.com>
2020-04-01 15:36:45 +0430

    Use expressjs for routing

--- src/main.js
+++ src/main.js
@@ -1,23 +1,27 @@
-// Import modules
+/**
+ * Import modules
+ */ 
 const http = require('http')
 const yaml = require('yaml')
 const fs = require('fs')
+const httpStatus = require('http-status-codes');
+const jose = require('jose');
+const express = require('express');
 const log = require("./services/logger.js")
 
+var app = express();
 /**
  * Read configs
  */
-const configContent = fs.readFileSync(__dirname+"/configs/config.yaml", 'utf8')
+const configContent = fs.readFileSync(__dirname + "/configs/config.yaml", 'utf8')
 const configs = yaml.parse(configContent)
 
 /**
  * Create server
  */
-http.createServer(function () {
-    try {
-        new log.l("hi");
-    } catch (error) {
-        log.e(error);
-    }
-}).listen(configs["port"]);
+
+app.get('/', function(req, res) {
+    res.send('Hello Sir')
+});
+app.listen(configs["port"]);
 log.l("Server running on http://127.0.0.1:" + configs["port"])
