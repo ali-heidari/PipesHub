908368bf53ae4719b48cfd508d29b77b793e4885
ali <ali-heidari@outlook.com>
2020-04-03 21:02:32 +0430

    custom view engine

--- src/modules/md_engine.js
+++ src/modules/md_engine.js
@@ -1,20 +1,20 @@
 const fs = require('fs');
-const converter = require('showdown').Converter();
-
 /**
  * The return object of controller
  */
 exports.setEngine = function (app) {
-    app.engine('mde', function (filePath, options, callback) { // define the template engine
+    app.engine('md', function (filePath, options, callback) {
         fs.readFile(filePath, function (err, content) {
-            if (err) return callback(err);
+            if (err)
+                return callback(err);
             // this is an extremely simple template engine
-            let md = content.toString()
-                .replace('#title#', options.title)
-                .replace('#message#', options.message);
-            html = converter.makeHtml(md);
-            return callback(null, html)
-        })
+            const md = content.toString()
+                .replace('?title?', options.title)
+                .replace('?message?', options.message);
+            const showdown = require('showdown');
+            let html = showdown.makeHtml(md);
+            return callback(null, html);
+        });
     });
-    app.set('view engine', 'mde');
+    app.set('view engine', 'md');
 }
