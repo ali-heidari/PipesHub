2770c8f1077efc4a31b934db944db2d9be3181e8
ali <ali-heidari@outlook.com>
2020-04-09 00:13:54 +0430

    keep alive connection

--- src/clients/app.js
+++ src/clients/app.js
@@ -1,36 +1,39 @@
 const http = require('http');
 
-const agent = new http.Agent({
-    keepAlive: true
-  });
+const agent = new http.Agent;
+agent.maxSockets = 1;
+agent.keepAlive = true;
 
-  console.log(agent)
+console.log(agent)
 
-var post_options = {
-    host: 'localhost',
-    port: '16916',
-    path: '/auth',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'keepAlive': true,
-        'connection':'keep-alive',
-    }
-};
+const request = function () {
+    var post_options = {
+        host: 'localhost',
+        port: '16916',
+        path: '/auth',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        agent: agent
+    };
 
-let post_req = http.request(post_options, function (res) {
-    console.log(res.headers)
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-        console.log('Response: ' + chunk);
+    let post_req = http.request(post_options, function (res) {
+        console.log(res.headers)
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('Response: ' + chunk);
+        });
+        res.on('end', () => {
+            console.log('end');
+            request();
+        });
     });
-    res.on('end', () => {
-        console.log('end');
-    });
-});
 
-// post the data
-post_req.write('sec=aaaa');
-post_req.end();
+    // post the data
+    post_req.write('sec=aaaa');
+    post_req.end();
 
-console.log('request sent');
+    console.log('request sent');
+}
+request();
