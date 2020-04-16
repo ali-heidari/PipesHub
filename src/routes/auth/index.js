bf20a4b2b36e6981a1e2dc0b7fbff0a0cb2c57c7
ali <ali-heidari@outlook.com>
2020-04-16 22:05:12 +0430

    mongoose added for authentication

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const auth = require("../../services/authenticator");
-
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {
+    useNewUrlParser: true
+});
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    var schema = new mongoose.Schema({
+        name: String
+    });
+    var user = mongoose.model('user', schema);
+    var guest = new user({
+        name: 'guest'
+    });
+    guest.save(function (err, fluffy) {
+        if (err) return console.error(err);
+    });
+});
 /**
  *  Login API 
  */
@@ -17,7 +34,10 @@ router.post('/', function (req, res, next) {
         console.log('Connection to client closed.');
         res.end();
     });
-    res.end(auth.sign('localhost', 'some-uid'));
+    Kitten.find({
+        name: `/^${req.body.name}/`
+    }, () => res.end(auth.sign('localhost', 'some-uid')));
+
 });
 
 module.exports = router;
