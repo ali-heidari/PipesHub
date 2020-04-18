f17bc81e72472c4a8a8d776fb971b678e3e80c2d
ali <ali-heidari@outlook.com>
2020-04-18 10:36:49 +0430

    Add user function

--- src/modules/data.js
+++ src/modules/data.js
@@ -9,17 +9,22 @@ const db = mongoose.connection;
 const schema = new mongoose.Schema({
     name: String
 });
+
 const user = mongoose.model('user', schema);
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
+    console.log('Connection to mongodb established.')
+});
+
+module.exports.addUser = (username) => {
     var guest = new user({
-        name: 'guest'
+        name: username
     });
     guest.save(function (err, guest) {
         if (err) return console.error(err);
         console.log(guest);
     });
-});
+};
 
 module.exports.findUser = (username) => user.find({
     name: `${username}`
