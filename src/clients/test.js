92d16527ea9493bf4c461a394885c60a7737a2c1
ali <ali-heidari@outlook.com>
2020-05-10 13:46:57 +0430

    hash_log added

--- src/clients/test.js
+++ src/clients/test.js
@@ -1,3 +1,5 @@
+const logger =require("../modules/logger")
+
 const App = require("./app").App;
 const Service = require("./service").Service;
 
@@ -6,32 +8,38 @@ exports.run = function () {
     let cs = new Service('cService');
 
 
-    cs.add('sum', (args) => args.pushResponse(args.a + args.b));
-    let theList = []
-    cs.add('add', (args) => theList.push(args.a));
-    cs.add('list', (args) => args.pushResponse(theList));
-    cs.add('message', (args) => setInterval(() => {
+    // cs.add('sum', (args) => args.pushResponse(args.a + args.b));
+    // let theList = []
+    // cs.add('add', (args) => theList.push(args.a));
+    // cs.add('list', (args) => args.pushResponse(theList));
+    // cs.add('message', (args) => setInterval(() => {
+    //     args.pushResponse("Time is " + new Date());
+    // }, 5000))
+    cs.add('log', (args) => setInterval(() => {
         args.pushResponse("Time is " + new Date());
     }, 5000))
 
+
     setTimeout(async () => {
-        let res = await ca.ask('cService', 'sum', {
-            a: 9,
-            b: 2
-        });
-        console.log(res.res);
+        // let res = await ca.ask('cService', 'sum', {
+        //     a: 9,
+        //     b: 2
+        // });
+        // console.log(res.res);
 
-        ca.request('cService', 'add', {
-            a: 555
-        });
-        ca.request('cService', 'add', {
-            a: "test"
-        });
+        // ca.request('cService', 'add', {
+        //     a: 555
+        // });
+        // ca.request('cService', 'add', {
+        //     a: "test"
+        // });
 
-        res = await ca.ask('cService', 'list', null);
-        console.log(res.res);
+        // res = await ca.ask('cService', 'list', null);
+        // console.log(res.res);
 
-        ca.persist('cService', 'message', null, data => console.log(data.res));
+        // ca.persist('cService', 'message', null, data => console.log(data.res));
+
+        ca.persist('cService', 'log', null, data => logger.hash_log(data.res));
 
     }, 100);
 }
--- src/modules/logger.js
+++ src/modules/logger.js
@@ -1,4 +1,10 @@
-class Logger {    
+const fs = require('fs');
+const crypto = require('crypto');
+
+class Logger {
+    indexer = 0;
+    lastHash = '';
+
     /**
      * Normal log
      * @param {String} message 
@@ -15,6 +21,44 @@ class Logger {
     e(exception) {
         console.error(exception);
     }
+
+
+    generate_hash(content) {
+        const hash_sha256 = crypto.createHash('sha256');
+        return hash_sha256.update(content, 'utf8').digest('hex');
+    }
+
+    /**
+     * Stores hashed format of log
+     * @param {String} message 
+     */
+    hash_log(message) {
+
+        // Generate hash for received data
+        let hash = this.generate_hash(this.lastHash.concat(message));
+
+        // Create log object to ensure fixed log structure 
+        const log_data = {
+            i: this.indexer,
+            log: message,
+            hash: hash
+        }
+
+        // Store stringified log data
+        let log = JSON.stringify(log_data);
+
+        // Write in log file
+        fs.appendFile('./system.log', log+'\n', function (err) {
+            if (err) return console.error(err);
+        });
+
+        // log on console 
+        this.l(log);
+
+        // Set variables for next log
+        this.indexer++;
+        this.lastHash = hash;
+    }
 }
 const logger = new Logger();
 /**
@@ -29,4 +73,7 @@ exports.l = (message) => {
 };
 exports.e = (exception) => {
     logger.e(exception)
+};
+exports.hash_log = (exception) => {
+    logger.hash_log(exception)
 };
